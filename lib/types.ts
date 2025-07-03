a38372613ebc22b99112f3e3348316bf21ee3618
Randriatseheno <rajostelly@gmail.com>
2025-07-03 19:28:34 +0200

    docs: type

--- lib/types.ts
+++ lib/types.ts
@@ -1,18 +1,19 @@
 export interface Ingredient {
-  id?: string
-  name: string
-  quantity: number
-  price: number
+  id?: string;
+  name: string;
+  quantity: number;
+  price: number;
 }
 
+// this is a TypeScript interface for a recipe object
 export interface Recipe {
-  id: string
-  name: string
-  description?: string
-  ingredients: Ingredient[]
-  protocol: string[]
-  molding: string[]
-  yield: number
-  totalCost: number
-  stock: number
+  id: string;
+  name: string;
+  description?: string;
+  ingredients: Ingredient[];
+  protocol: string[];
+  molding: string[];
+  yield: number;
+  totalCost: number;
+  stock: number;
 }
