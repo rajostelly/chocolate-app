741fddeae8865547aa2b792ad38fb8ad330b680c
Randriatseheno <rajostelly@gmail.com>
2025-07-09 18:10:04 +0200

    docs : add js doc cn function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Combines class names using clsx and merges Tailwind CSS classes using twMerge.
+ *
+ * @param inputs - Class names to be combined.
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
