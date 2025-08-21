aab6060edecace043f544df5ce0d69d54d81bb6a
Randriatseheno <rajostelly@gmail.com>
2025-08-21 21:15:44 +0200

    docs: add comment cn function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// add cn function
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
